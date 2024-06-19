54eb642ddd5254ae40851a964f899943518bc9d9
Gabriel Belouze <gabriel@belouze.com>
2024-06-19 15:04:20 +0200

    Keep cli module as part of geefetch

--- src/geefetch/__init__.py
+++ src/geefetch/__init__.py
@@ -4,10 +4,4 @@ geefetch
 A python library/CLI to download large scale satellite data from Google Earth Engine.
 """
 
-from __future__ import annotations
-
-from . import coords, data, utils
-
-__all__ = ["coords", "data", "utils"]
-
-__version__ = "0.2"
+__version__ = "0.1.0"
