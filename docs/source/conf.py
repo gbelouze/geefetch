b22d4ffdbe923382e2419e56b3998c2edc975da6
Gabriel Belouze <gabriel@belouze.com>
2024-10-31 16:18:49 +0100

    Add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,4 +36,4 @@ autodoc_default_options = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_static_path = ["../../assets"]
