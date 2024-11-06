17afcc9ac6903870e3e49383530849f3f577f172
Gabriel Belouze <61082441+gbelouze@users.noreply.github.com>
2024-11-06 16:04:41 +0100

    Add GEEFETCH_DEBUG option to disable progress bars (#28)

--- src/geefetch/utils/progress.py
+++ src/geefetch/utils/progress.py
@@ -1,3 +1,6 @@
+import logging
+import os
+
 from rich.progress import (
     BarColumn,
     MofNCompleteColumn,
@@ -6,14 +9,35 @@ from rich.progress import (
     TimeElapsedColumn,
 )
 
-__all__ = ["default_bar"]
+__all__ = ["default_bar", "disable_progress"]
+
+
+log = logging.getLogger(__file__)
+
+
+def disable_progress() -> bool:
+    match os.getenv("GEEFETCH_DEBUG"):
+        case "true":
+            return True
+        case "1":
+            return True
+        case _:
+            return False
 
 
 def default_bar() -> Progress:
+    disabled = disable_progress()
+    if disabled:
+        log.warn("progress bar is disabled.")
     return Progress(
         TextColumn("[progress.description]{task.description}"),
         BarColumn(),
         MofNCompleteColumn(),
         TimeElapsedColumn(),
         refresh_per_second=1,
+        disable=disabled,
     )
+
+
+if __name__ == "__main__":
+    print(disable_progress())
