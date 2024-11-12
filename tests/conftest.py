2c12ebc261617f864e7f8a996fd0725d2d46c731
Gabriel Belouze <gabriel@belouze.com>
2024-11-12 18:24:12 +0100

    Add end-to-end tests

---
+++ tests/conftest.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+
+import pytest
+from omegaconf import DictConfig, OmegaConf
+
+from geefetch.cli.omegaconfig import GeefetchConfig, load
+
+TESTS_DIR = Path(__file__).parent
+GEE_PROJECT_ID_ENV_NAME = "GEEFETCH_GEE_PROJECT_ID"
+
+
+@pytest.fixture
+def raw_paris_config() -> DictConfig:
+    return OmegaConf.load(TESTS_DIR / "data" / "paris_config.yaml")
+
+
+@pytest.fixture(scope="session")
+def gee_project_id() -> str:
+    match os.getenv(GEE_PROJECT_ID_ENV_NAME):
+        case None:
+            pytest.fail(
+                f"Did not find {GEE_PROJECT_ID_ENV_NAME} in the environment. Cannot query Google Earth Engine."
+            )
+        case _ as project_id:
+            assert isinstance(project_id, str)
+            return project_id
+
+
+@pytest.fixture
+def paris_config_path(
+    raw_paris_config: DictConfig, tmp_path: Path, gee_project_id: str
+) -> Path:
+    raw_paris_config.data_dir = str(tmp_path)
+    raw_paris_config.satellite_default.gee.ee_project_id = gee_project_id
+
+    conf_path = tmp_path / "config.yaml"
+    with open(conf_path, "w+") as conf_file:
+        conf_file.write(OmegaConf.to_yaml(raw_paris_config))
+    return conf_path
+
+
+@pytest.fixture
+def paris_config(paris_config_path: Path) -> GeefetchConfig:
+    return load(paris_config_path)
---
+++ tests/test_cli.py
@@ -0,0 +1,33 @@
+import logging
+from pathlib import Path
+
+from click.testing import CliRunner
+
+from geefetch.cli.main import main
+
+log = logging.getLogger("geefetch")
+
+
+TESTS_DIR = Path(__file__).parent
+
+
+def test_api_consistency():
+    runner = CliRunner()
+    res = runner.invoke(main, ["--help"])
+    assert res.exit_code == 0
+    with open(TESTS_DIR / "data" / "cli_help.txt", "r") as help_msg:
+        assert res.output == help_msg.read()
+
+
+def test_fail_on_missing_config():
+    runner = CliRunner(mix_stderr=False)
+    results = [
+        runner.invoke(main, [satellite, "--config", "fakeconfig.yaml"])
+        for satellite in ["s1", "s2", "palsar2", "gedi", "dynworld", "landsat8"]
+    ]
+    for res in results:
+        assert res.exit_code == 2
+        assert (
+            "Error: Invalid value for '--config' / '-c': Path 'fakeconfig.yaml' does not exist."
+            in res.stderr
+        )
