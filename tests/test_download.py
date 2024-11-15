f55cd56bd5980c01cd274ad39d159ebf4924b353
Gabriel Belouze <61082441+gbelouze@users.noreply.github.com>
2024-11-15 18:11:47 +0100

    Feat/test timeseries (#31)
    * Test timeseries
    
    * Mark tests slow if they are slow and are likely to pass
    
    * Change default pytest log verbosity to DEBUG

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pytest
+
 from geefetch.cli.download_implementation import (
     download_dynworld,
     download_gedi,
@@ -14,25 +16,64 @@ def test_download_s1(paris_config_path: Path):
     download_s1(paris_config_path)
 
 
+def test_download_timeseries_s1(paris_timeseriesconfig_path: Path):
+    download_s1(paris_timeseriesconfig_path)
+
+
+@pytest.mark.slow
 def test_download_s2(paris_config_path: Path):
     download_s2(paris_config_path)
 
 
+@pytest.mark.slow
+def test_download_timeseries_s2(paris_timeseriesconfig_path: Path):
+    download_s2(paris_timeseriesconfig_path)
+
+
+@pytest.mark.slow
 def test_download_dynworld(paris_config_path: Path):
     download_dynworld(paris_config_path)
+
+
+@pytest.mark.slow
+def test_download_timeseries_dynworld(paris_timeseriesconfig_path: Path):
+    download_dynworld(paris_timeseriesconfig_path)
 
 
 def test_download_gedi_vector(paris_config_path: Path):
     download_gedi(paris_config_path, vector=True)
 
 
+@pytest.mark.slow
+def test_download_timeseries_gedi_vector(paris_timeseriesconfig_path: Path):
+    download_gedi(paris_timeseriesconfig_path, vector=True)
+
+
+@pytest.mark.slow
 def test_download_gedi_raster(paris_config_path: Path):
     download_gedi(paris_config_path, vector=False)
 
 
+@pytest.mark.slow
+def test_download_timeseries_gedi_raster(paris_timeseriesconfig_path: Path):
+    download_gedi(paris_timeseriesconfig_path, vector=False)
+
+
+@pytest.mark.slow
 def test_download_landsat8(paris_config_path: Path):
     download_landsat8(paris_config_path)
 
 
+@pytest.mark.slow
+def test_download_timeseries_landsat8(paris_timeseriesconfig_path: Path):
+    download_landsat8(paris_timeseriesconfig_path)
+
+
+@pytest.mark.slow
 def test_download_palsar2(paris_config_path: Path):
     download_palsar2(paris_config_path)
+
+
+@pytest.mark.slow
+def test_download_timeseries_palsar2(paris_timeseriesconfig_path: Path):
+    download_palsar2(paris_timeseriesconfig_path)
