fdf1086d48aaa71f41f368385c5ab08fc93b40d9
Gabriel Belouze <gabriel@belouze.com>
2024-12-06 13:11:57 +0100

    Update for latest ee.Authenticate API

--- src/geefetch/utils/gee.py
+++ src/geefetch/utils/gee.py
@@ -16,3 +16,18 @@ def auth(project: str) -> None:
         project=project,
         opt_url="https://earthengine-highvolume.googleapis.com",
     )
+
+
+def patched_auth(project: str) -> None:
+    """Authentificate and initialize Google Earth Engine
+
+    Parameters
+    ----------
+    project : str
+        Google Earth Engine project id.
+    """
+    ee.Authenticate()
+    ee.Initialize(
+        project=project,
+        opt_url="https://earthengine-highvolume.googleapis.com",
+    )
