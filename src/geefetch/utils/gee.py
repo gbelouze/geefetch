209d43fb66d5d0697724cfd311c45b78c4b940b1
Gabriel Belouze <gabriel@belouze.com>
2025-02-13 17:52:55 +0100

    Remove junk code

--- src/geefetch/utils/gee.py
+++ src/geefetch/utils/gee.py
@@ -16,18 +16,3 @@ def auth(project: str) -> None:
         project=project,
         opt_url="https://earthengine-highvolume.googleapis.com",
     )
-
-
-def patched_auth(project: str) -> None:
-    """Authentificate and initialize Google Earth Engine
-
-    Parameters
-    ----------
-    project : str
-        Google Earth Engine project id.
-    """
-    ee.Authenticate()
-    ee.Initialize(
-        project=project,
-        opt_url="https://earthengine-highvolume.googleapis.com",
-    )
